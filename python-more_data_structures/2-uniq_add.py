90c580c0011c82174399ad7daf3b1fe1f7782ba2
Levis Ishimwe <i.levis@alustudent.com>
2024-06-25 14:12:39 +0200

    Update 2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    uniq_list = list()
-    for item in my_list:
-    if item not in uniq_list:
-    uniq_list.append(item)
-    return sum(uniq_list)
+    return sum(set(my_list))
