6a35fc2ebdce0523bf9b3fdbd832c2f6991aed94
Levis Ishimwe <i.levis@alustudent.com>
2024-06-25 14:54:17 +0200

    Update 8-simple_delete.py

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    return {k: v * 2 for k, v in a_dictionary.items()}
+def simple_delete(a_dictionary, key=""):
+    a_dictionary.pop(key, None)
+    return a_dictionary
