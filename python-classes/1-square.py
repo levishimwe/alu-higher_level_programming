16c841473faffa0ca1e527125c345810d7339867
levishimwe <i.levis@alustudent.com>
2024-07-04 07:05:01 -0700

    Lord God is very amazing

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""""define class square"""
+
+class square:
+
+    """display square"""
+
+    def __init__(self,size):
+
+        """initialise a new square.
+        Args:
+
+        size (int):the size of new square"""
+
+        self.size= size
