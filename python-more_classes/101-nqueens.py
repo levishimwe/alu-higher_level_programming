3ac59daf2b6782b40b60649cbfe3282b8dce04fe
levishimwe <i.levis@alustudent.com>
2024-07-06 11:10:26 -0700

    I LOVE YOU LORD

---
+++ python-more_classes/101-nqueens.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import sys
+
+def print_usage_and_exit():
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+def solve_nqueens(N):
+    def is_safe(board, row, col):
+        for i in range(col):
+            if board[row][i] == 1:
+                return False
+        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+        for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+            if board[i][j] == 1:
+                return False
+        return True
+
+    def solve(board, col, solutions):
+        if col >= N:
+            solutions.append([[i, row.index(1)] for i, row in enumerate(board)])
+            return
+        for i in range(N):
+            if is_safe(board, i, col):
+                board[i][col] = 1
+                solve(board, col + 1, solutions)
+                board[i][col] = 0
+
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solutions = []
+    solve(board, 0, solutions)
+    return solutions
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print_usage_and_exit()
+    
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    
+    solutions = solve_nqueens(N)
+    for solution in solutions:
+        print(solution)
