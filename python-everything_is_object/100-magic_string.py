751404bbb2e4ff4811f9b80c716c407e2f0dd904
Levis Ishimwe <i.levis@alustudent.com>
2024-07-09 15:47:48 +0200

    Update 100-magic_string.py

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
     def magic_string(n):
     return "BestSchool" * (n * 2)
-print(magic_string())  # Call the function here
+    print(magic_string(n))  # Call the function here
