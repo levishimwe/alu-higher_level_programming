cd5fdc34371ea3fac1d333b476a37aff8c7a49b3
Levis Ishimwe <i.levis@alustudent.com>
2024-07-09 16:20:59 +0200

    Update 101-locked_class.py

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-class LockedClass:
-    __slots__ = ['first_name']
-locked_obj = LockedClass()
-locked_obj.first_name = "John"  # This works
-    print(locked_obj.first_name)  # Outputs: John
 
-try:
-    locked_obj.last_name = "Doe"  # This will raise an AttributeError
-except AttributeError as e:
-    print(f"Error: {e}")
+"""Module that defines a locked class."""
+
+
+class LockedClass:
+    """
+    A class that prevents dynamic creation of new instance attributes,
+    except for 'first_name'.
+    """
+    __slots__ = ['first_name']
