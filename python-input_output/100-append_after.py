4159a98abca681d36752de212388bed602238bbe
Levis Ishimwe <i.levis@alustudent.com>
2024-07-17 14:26:38 +0200

    Update 100-append_after.py

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file, after each line containing a specific string.
+
+    Args:
+    filename (str): The name of the file.
+    search_string (str): The string to search for within each line of the file.
+    new_string (str): The string to append after each line that contains the search string.
+
+    """
     # Read the file content
     with open(filename, 'r') as file:
         lines = file.readlines()
@@ -10,4 +19,7 @@ def append_after(filename="", search_string="", new_string=""):
         for line in lines:
             file.write(line)
             if search_string in line:
-                file.write(new_string + '\n')
+                file.write(new_string)
+
+# Example usage:
+# append_after("example.txt", "world", "This is the appended line")
