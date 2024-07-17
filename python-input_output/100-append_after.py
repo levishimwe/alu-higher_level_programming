233bf299066f2227164b07784213ad8f1ceec287
Levis Ishimwe <i.levis@alustudent.com>
2024-07-17 14:41:10 +0200

    Update 100-append_after.py

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 """Are you docuemnted ?"""
+
 def append_after(filename="", search_string="", new_string=""):
     """
     Insert a line of text after each a specific string in a file.
     Args:
     filename (str): The name of the file to modify.
     search_string (str): The string to search for in each line.
-    new_string (str): The string to insert after each line containing the search string.
-
     Returns:
     None
-
     This function reads the content of the specified file, and inserts the new string after each matching line.
-    The modified content is then written back to the file.
-
     Note:
     - The function uses the 'with' statement for file handling.
     - It does not manage file permissions or 'file doesn't exist' exceptions.
