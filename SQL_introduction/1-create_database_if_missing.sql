e85eb95d9bdb5b779bbd267f83adddf5088f2552
Levis Ishimwe <i.levis@alustudent.com>
2024-07-18 15:08:43 +0200

    JESUS MAKE ME DATA KEEPER

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,3 @@
---let me database hbtn_0c_0 in my MySQL server.
---while database hbtn_0c_0 already exist should not fail.
- CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
- 
+--  Creates the database 'hbtn_0c_0'
+-- If database already exists, the script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
