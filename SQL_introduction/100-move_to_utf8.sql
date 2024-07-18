759c607f9bae5aa7464892062c0387a6b722bd13
Levis Ishimwe <i.levis@alustudent.com>
2024-07-18 17:22:59 +0200

    Update 100-move_to_utf8.sql

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -7,5 +7,5 @@ USE hbtn_0c_0;
 -- Convert the table
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the name field
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the name field (removed explicit CHARACTER SET)
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
