02fc2e4e873913a3b1936ead7faa876538bc111e
Levis Ishimwe <i.levis@alustudent.com>
2024-07-18 17:25:34 +0200

    JESUS MAKE ME DATA KEEPER

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,11 +1,7 @@
 -- Convert the database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
 -- Select the database
 USE hbtn_0c_0;
-
--- Convert the table
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the name field (removed explicit CHARACTER SET)
-ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+-- Convert the table and all its fields in one command
+ALTER TABLE first_table 
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
