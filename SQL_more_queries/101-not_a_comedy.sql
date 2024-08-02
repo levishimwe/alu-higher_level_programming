e910103b2c872dcc24eac276c54c040cb1bb0480
Levis Ishimwe <i.levis@alustudent.com>
2024-08-02 14:07:37 +0200

    changes

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -2,9 +2,8 @@
 SELECT DISTINCT tv_shows.title
 FROM tv_shows
 WHERE tv_shows.id NOT IN
-    (SELECT tv_shows.id
-     FROM tv_shows
-     INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-     INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    (SELECT tv_show_genres.show_id
+     FROM tv_show_genres
+     JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
      WHERE tv_genres.name = 'Comedy')
 ORDER BY tv_shows.title ASC;
