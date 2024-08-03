ff6117109805e1f3fea3ed4609dc2ce07b4f1b77
Levis Ishimwe <i.levis@alustudent.com>
2024-08-03 19:05:09 +0200

    excellent in python

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all State objects and corresponding City objects contained in the DB
+display all State objects and corresponding City objects contained in the DB
 """
 import sys
 from relationship_state import Base, State
